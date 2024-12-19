38629f71fcf7de480a4249e03af2c9a80f35b743
Pouria Khalaj <pouria.khalaj@smhi.se>
2024-12-19 14:32:48 +0100

    Make the functions in the `generic` sub-package more robust in terms of handling different types

--- monkey_wrench/generic/_common.py
+++ monkey_wrench/generic/_common.py
@@ -6,30 +6,41 @@ from pydantic import validate_call
 
 
 @validate_call
-def apply_to_single_or_all(func: Callable, single_item_or_items: Any) -> Any:
-    """Apply the given function to all items of the input (if it is a list/tuple), or only on the single input.
+def apply_to_single_or_all(func: Callable, single_item_or_items: list | dict | set | tuple | Any) -> Any:
+    """Apply the given function to all items of the input (if it is a list/dict/set/tuple), or only on the single input.
+
+    Note:
+        In case of a dictionary, the func will be applied on the values.
 
     Args:
         func:
             The function to be applied.
         single_item_or_items:
-            Either a single item or a list/tuple of items.
+            Either a single item or a list/dict/set/tuple of items.
 
     Returns:
-        Either a single output or a list of outputs, resulting from applying the given function.
+        Either a single output or a list/dict/set/tuple of outputs, resulting from applying the given function.
 
     Example:
         >>> from monkey_wrench.generic import apply_to_single_or_all
         >>> apply_to_single_or_all(lambda x: x**2, [1, 2, 3])
         [1, 4, 9]
         >>> apply_to_single_or_all(lambda x: x**2, (1, 2, 3))
-        [1, 4, 9]
+        (1, 4, 9)
+        >>> apply_to_single_or_all(lambda x: x**2, {"a": 1, "b": 2, "c":3})
+        {"a": 1, "b": 4, "c": 9}
         >>> apply_to_single_or_all(lambda x: x**2, 3)
         9
     """
     match single_item_or_items:
-        case list() | tuple():
+        case list():
             return [func(i) for i in single_item_or_items]
+        case set():
+            return {func(i) for i in single_item_or_items}
+        case tuple():
+            return tuple([func(i) for i in single_item_or_items])
+        case dict():
+            return {k: func(v) for k, v in single_item_or_items.items()}
         case _:
             return func(single_item_or_items)
 
@@ -38,13 +49,21 @@ def apply_to_single_or_all(func: Callable, single_item_or_items: Any) -> Any:
 def return_single_or_first(single_item_or_items: Any) -> Any:
     """Return the first item from the input if it is a list/tuple, otherwise return the input itself.
 
+    Warning:
+        This function raises an exception for sets and dictionaries, as they are not ordered and the concept of the
+        first element does not apply.
+
     Args:
         single_item_or_items:
             Either a single item or a list/tuple of items.
 
     Returns:
-        Either the given single input as it is, or the first item from the input if it is a list/tuple. In case of a
-        falsy item such as an empty list ``[]``, the falsy item will be returned as it is.
+        Either the given single input as it is, or the first item from the input if it is a list/tuple. An empty
+        list/tuple will be returned as it is.
+
+    Raises:
+        ValueError:
+            If the given input is a set or a dictionary.
 
     Example:
         >>> from monkey_wrench.generic import return_single_or_first
@@ -57,10 +76,12 @@ def return_single_or_first(single_item_or_items: Any) -> Any:
         >>> return_single_or_first([])
         []
     """
-    if not single_item_or_items:
+    if single_item_or_items in [[], ()]:
         return single_item_or_items
 
     match single_item_or_items:
+        case set() | dict():
+            raise ValueError("Cannot return the first item from a `set` or a `dictionary`.")
         case list() | tuple():
             return single_item_or_items[0]
         case _:
--- tests/generic/test_common.py
+++ tests/generic/test_common.py
@@ -5,10 +5,14 @@ from monkey_wrench.generic import apply_to_single_or_all, return_single_or_first
 
 @pytest.mark.parametrize(("inp", "out"), [
     ([1, 2, 3], [1, 4, 9]),
-    ((1, 2, 3), [1, 4, 9]),
+    ((1, 2, 3), (1, 4, 9)),
+    ({1, 2, 3}, {1, 4, 9}),
     (3, 9),
     ([3], [9]),
-    ([], [])
+    ([], []),
+    ({}, {}),
+    (tuple(), tuple()),
+    ({"a": 1, "b": 2}, {"a": 1, "b": 4})
 ])
 def test_apply_to_single_or_all(inp, out):
     assert apply_to_single_or_all(lambda x: x ** 2, inp) == out
@@ -21,6 +25,21 @@ def test_apply_to_single_or_all(inp, out):
     ([], []),
     ((), ()),
     (None, None),
+    ("abc", "abc")
 ])
 def test_return_single_or_first(inp, out):
     assert return_single_or_first(inp) == out
+
+
+@pytest.mark.parametrize("inp", [
+    set(),
+    {1},
+    {1, 2, 3},
+    {},
+    dict(),
+    {1: 1},
+    {1: 1, 2: 2}
+])
+def test_return_single_or_first_raise(inp):
+    with pytest.raises(ValueError, match="Cannot return"):
+        return_single_or_first(inp)
