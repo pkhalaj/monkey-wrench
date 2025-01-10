ed22986a42bece54b5be3b13e689ba3d0599e024
Pouria Khalaj <pouria.khalaj@smhi.se>
2025-01-10 21:54:02 +0100

    Improve test coverage for the `cli` package

--- tests/cli/test_common.py
+++ tests/cli/test_common.py
@@ -1,21 +1,24 @@
 from collections import namedtuple
 from unittest import mock
+from unittest.mock import MagicMock
 
 from monkey_wrench.cli import run
 from monkey_wrench.task import InputFile
-from tests.utils import CLIArguments, noop
+from tests.utils import CLIArguments
 
 
 def test_run(empty_task_filepath):
     Task = namedtuple("Task", ["perform"])
+    task = Task(perform=MagicMock())
 
     func = dict(
         path="monkey_wrench.cli._common.read_tasks_from_file",
         arg=InputFile(input_filename=empty_task_filepath),
-        return_value=[Task(perform=noop)]
+        return_value=[task, task],
     )
 
     with CLIArguments(empty_task_filepath):
         with mock.patch(func["path"], return_value=func["return_value"]) as read_tasks_from_file:
             run()
             read_tasks_from_file.assert_called_once_with(func["arg"])
+            assert 2 == task.perform.call_count
