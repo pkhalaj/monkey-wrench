586d3b4f47cb7a59d1742e799003287c1ad5e1d8
Pouria Khalaj <pouria.khalaj@smhi.se>
2025-02-09 15:20:58 +0100

    Refactor `ids` tasks

--- monkey_wrench/task/ids.py
+++ monkey_wrench/task/ids.py
@@ -5,8 +5,8 @@ from typing import Literal
 from pydantic import NonNegativeInt
 
 from monkey_wrench.date_time import DateTimeRangeInBatches
-from monkey_wrench.input_output import OutputFile, write_items_to_txt_file_in_batches
-from monkey_wrench.query import EumetsatAPI
+from monkey_wrench.input_output import Writer
+from monkey_wrench.query import EumetsatQuery
 from monkey_wrench.task.base import Action, Context, TaskBase
 
 
@@ -15,7 +15,7 @@ class Task(TaskBase):
     context: Literal[Context.product_ids]
 
 
-class FetchSpecifications(DateTimeRangeInBatches, OutputFile):
+class FetchSpecifications(DateTimeRangeInBatches, Writer):
     """Pydantic base model for the specifications of a fetch task."""
     pass
 
@@ -28,17 +28,8 @@ class Fetch(Task):
     @TaskBase.log
     def perform(self) -> dict[str, NonNegativeInt]:
         """Fetch the product IDs."""
-        api = EumetsatAPI()
-
-        product_batches = api.query_in_batches(
-            start_datetime=self.specifications.start_datetime,
-            end_datetime=self.specifications.end_datetime,
-            batch_interval=self.specifications.batch_interval
-        )
-        number_of_items = write_items_to_txt_file_in_batches(
-            product_batches,
-            self.specifications.output_filename
-        )
+        product_batches = EumetsatQuery().query_in_batches(self.specifications.datetime_range_in_batches)
+        number_of_items = self.specifications.write_in_batches(product_batches)
 
         return {
             "number of items successfully fetched and written to the file": number_of_items,
--- tests/task/test_product_ids.py
+++ tests/task/test_product_ids.py
@@ -5,44 +5,39 @@ import pytest
 from pydantic import ValidationError
 
 from monkey_wrench.date_time import SeviriIDParser
-from monkey_wrench.input_output import read_items_from_txt_file
-from monkey_wrench.task import InputFile, read_tasks_from_file
+from monkey_wrench.input_output import ExistingInputFile, Reader
+from monkey_wrench.task import read_tasks_from_file
 from tests.task.const import (
-    BATCH_INTERVAL,
-    END_DATETIME,
-    FUTURE_DATETIME,
-    OUTPUT_FILENAME,
-    START_DATETIME,
+    batch_interval,
+    end_datetime,
+    future_datetime,
+    output_filepath,
     specification_with,
+    start_datetime,
     task,
     task_with,
 )
 from tests.utils import make_yaml_file
 
 
-def test_model_product_ids_success(temp_dir):
+def test_model_product_ids(temp_dir):
     filename = Path(temp_dir, "task.yaml")
     make_yaml_file(filename, task)
-    validated_task = list(read_tasks_from_file(InputFile(input_filename=filename)))[0]
-
+    validated_task = list(read_tasks_from_file(ExistingInputFile(input_filepath=filename)))[0]
     assert task["context"] == validated_task.context
     assert task["action"] == validated_task.action
 
-    assert datetime(*START_DATETIME) == validated_task.specifications.start_datetime
-    assert datetime(*END_DATETIME) == validated_task.specifications.end_datetime
-    assert timedelta(**BATCH_INTERVAL) == validated_task.specifications.batch_interval
-    assert Path(OUTPUT_FILENAME).absolute() == validated_task.specifications.output_filename
+    assert start_datetime == validated_task.specifications.start_datetime
+    assert end_datetime == validated_task.specifications.end_datetime
+    assert timedelta(**batch_interval) == validated_task.specifications.batch_interval
+    assert Path(output_filepath).absolute() == validated_task.specifications.output_filepath
 
 
 @pytest.mark.parametrize(("task", "error_message"), [
-    (specification_with(start_datetime=1), "must be a list"),
-    (specification_with(start_datetime=["A"]), "must be integer"),
-    (specification_with(end_datetime=[-1]), "must be positive"),
-    (specification_with(start_datetime=FUTURE_DATETIME), "should be in the past"),
-    (specification_with(end_datetime=FUTURE_DATETIME), "should be in the past"),
-    (specification_with(start_datetime=END_DATETIME, end_datetime=START_DATETIME), "is later than"),
-    (specification_with(output_filename="./this_is_directory_like/"), "cannot end with a '/'"),
-    (specification_with(output_filename=__file__), "already exists"),
+    (specification_with(start_datetime=["A"]), "datetime"),
+    (specification_with(end_datetime=[1]), "datetime"),
+    (specification_with(end_datetime=datetime(2000, 1, 1)), "timezone"),
+    (specification_with(start_datetime=future_datetime), "future"),
     (task | dict(extra_argument="extra arguments are not allowed!"), "Extra inputs"),
     (task_with(context="non_existent_context"), "Input should be"),
     (task_with(action="non_existent_action"), "Input should be"),
@@ -51,18 +46,17 @@ def test_model_product_ids_raise(temp_dir, task, error_message):
     filename = Path(temp_dir, "task.yaml")
     make_yaml_file(filename, task)
     with pytest.raises(ValidationError, match=error_message):
-        list(read_tasks_from_file(InputFile(input_filename=filename)))
+        list(read_tasks_from_file(ExistingInputFile(input_filepath=filename)))
 
 
 def test_fetch_product_ids_success(get_token_or_skip, temp_dir):
     filename = Path(temp_dir, "task.yaml")
     output_filename = Path(temp_dir, "products_ids.txt")
-    make_yaml_file(filename, specification_with(output_filename=str(output_filename)))
+    make_yaml_file(filename, specification_with(output_filepath=str(output_filename)))
 
-    validated_task = list(read_tasks_from_file(InputFile(input_filename=filename)))[0]
+    validated_task = list(read_tasks_from_file(ExistingInputFile(input_filepath=filename)))[0]
     validated_task.perform()
-    data = read_items_from_txt_file(validated_task.specifications.output_filename)
-    start = datetime(*START_DATETIME)
-    end = datetime(*END_DATETIME)
+
+    data = Reader(input_filepath=validated_task.specifications.output_filepath).read()
     for product in data:
-        assert start <= SeviriIDParser.parse(product) < end
+        assert start_datetime <= SeviriIDParser.parse(product) < end_datetime
