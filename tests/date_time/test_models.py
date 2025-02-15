9d65d85a3dc5b26caf8385b53df14f4a3f9dd10d
Pouria Khalaj <11797985+pkhalaj@users.noreply.github.com>
2025-02-15 22:13:24 +0100

    Improve test coverage for `date_time`

--- tests/date_time/test_models.py
+++ tests/date_time/test_models.py
@@ -4,8 +4,10 @@ from zoneinfo import ZoneInfo
 import pytest
 from pydantic import ValidationError
 
-from monkey_wrench.date_time import AwarePastDateTime, TimeDeltaDict, TimeInterval
+from monkey_wrench.date_time import AwarePastDateTime, DateTimePeriod, TimeDeltaDict, TimeInterval
 from monkey_wrench.generic import Specifications
+
+from .const import end_datetime, start_datetime
 
 # ======================================================
 ### Tests for TimeDeltaDict()
@@ -71,3 +73,13 @@ def test_PastDateTime_raise(dt):
 
     with pytest.raises(ValidationError, match="valid"):
         Test(dt=dt)
+
+
+# ======================================================
+### Tests for DateTimePeriod()
+
+def test_DateTimePeriod():
+    datetime_period = DateTimePeriod(start_datetime=end_datetime, end_datetime=start_datetime)
+    assert datetime_period.span == start_datetime - end_datetime
+    assert datetime_period.as_tuple() == (end_datetime, start_datetime)
+    assert datetime_period.as_tuple(sort=True) == (start_datetime, end_datetime)
