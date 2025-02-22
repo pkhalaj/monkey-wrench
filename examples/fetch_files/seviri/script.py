a34ee345cd5c50c7cd5ad9ab079f77d9395db177
Pouria Khalaj <pouria.khalaj@smhi.se>
2025-02-22 19:21:13 +0100

    Update `fetch_files/seviri` examples

--- examples/fetch_files/seviri/script.py
+++ examples/fetch_files/seviri/script.py
@@ -1,44 +1,74 @@
-from datetime import datetime
 from pathlib import Path
 
-from monkey_wrench.date_time import SeviriIDParser
-from monkey_wrench.input_output import create_datetime_directory, read_items_from_txt_file, seviri
-from monkey_wrench.process import run_multiple_processes
-from monkey_wrench.query import EumetsatAPI, List
+from satpy.readers.seviri_base import CHANNEL_NAMES
 
-# Check here!
-product_ids_filename = Path("<replace-with-full-path-and-filename-of-the-text-file-in-which-product-ids-are-stored>")
-output_directory = Path("<replace-with-directory-where-the-files-are-to-be-stored>")
+from monkey_wrench.date_time import DateTimePeriod, SeviriIDParser
+from monkey_wrench.input_output import Reader
+from monkey_wrench.input_output.seviri import Resampler, input_filename_from_product_id
+from monkey_wrench.process import MultiProcess
+from monkey_wrench.query import List
 
-start_datetime = datetime(2022, 1, 1)
-end_datetime = datetime(2024, 1, 1)
+input_filepath = Path("<replace_with_the_full_path_of_the_text_file_in_which_product_ids_are_stored>")
+parent_directory_path = Path("<replace_with_the_path_of_the_top_level_directory_where_the_files_are_to_be_stored>")
+temp_directory_path = Path("<replace_with_the_directory_path_where_the_temp_files_are_to_be_stored>")
 
-product_ids = List(
-    read_items_from_txt_file(product_ids_filename),
-    SeviriIDParser
-).query(
-    start_datetime,
-    end_datetime
+datetime_period = DateTimePeriod(
+    start_datetime="2019-01-01T00:00:00+00:00",
+    end_datetime="2021-01-01T00:00:00+00:00"
 )
 
-
-def fetch(product_id):
-    """Fetch and resample the file with the given product ID."""
-    api = EumetsatAPI()
-    fs_file = api.open_seviri_native_file_remotely(product_id, cache="filecache")
-    datetime_directory = create_datetime_directory(
-        SeviriIDParser.parse(product_id),
-        parent=output_directory,
-        dry_run=True
+area = dict(
+    CHIMP_NORDIC_4=dict(
+        description="CHIMP region of interest over the nordic countries",
+        projection=dict(
+            proj="stere",
+            lat_0=90,
+            lat_ts=60,
+            lon_0=14,
+            x_0=0,
+            y_0=0,
+            datum="WGS84",
+            no_defs=None,
+            type="crs"),
+        shape=dict(
+            height=564,
+            width=452
+        ),
+        area_extent=dict(
+            lower_left_xy=[-745322.8983833211, -3996217.269197446],
+            upper_right_xy=[1062901.0232376591, -1747948.2287755085],
+            units="m"
+        )
     )
-    seviri.resample_seviri_native_file(
-        fs_file,
-        datetime_directory,
-        seviri.input_filename_from_product_id
-    )
+)
 
+resampler = Resampler(
+    fsspec_cache="filecache",
+    radius_of_influence=20_000,
+    remove_file_if_exists=True,
+    channel_names=list(CHANNEL_NAMES.values()),
+    dataset_save_options=dict(
+        writer="cf",
+        include_lonlats=False
+    ),
+    parent_directory_path=parent_directory_path,
+    output_filename_generator=input_filename_from_product_id,
+    area=area,
+)
+
+product_ids = List(
+    Reader(input_filepath=input_filepath).read(),
+    SeviriIDParser
+).query(
+    datetime_period
+)
 
 for product_id in product_ids:
-    create_datetime_directory(SeviriIDParser.parse(product_id), parent=output_directory)
+    resampler.create_datetime_directory(SeviriIDParser.parse(product_id))
 
-run_multiple_processes(fetch, product_ids, number_of_processes=2)
+if __name__ == "__main__":
+    MultiProcess(number_of_processes=4).run(
+        resampler.resample,
+        product_ids.to_python_list(),
+        temp_directory_path,
+    )
