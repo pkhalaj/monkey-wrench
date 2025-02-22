a8d6bb211fedc80cb67e315d24c85d1d17032233
Pouria Khalaj <pouria.khalaj@smhi.se>
2025-02-22 19:39:19 +0100

    Update `fetch_product_ids` examples

--- examples/fetch_product_ids/script.py
+++ examples/fetch_product_ids/script.py
@@ -1,22 +1,20 @@
-from datetime import datetime, timedelta
+from datetime import timedelta
 from pathlib import Path
 
-from query import EumetsatCollection
+from monkey_wrench.date_time import DateTimeRangeInBatches
+from monkey_wrench.input_output import Writer
+from monkey_wrench.query import EumetsatQuery
 
-from monkey_wrench.input_output import write_items_to_txt_file_in_batches
-from monkey_wrench.query import EumetsatAPI
+output_filepath = Path("<replace_with_the_full_path_of_the_text_file_in_which_product_ids_are_to_be_stored>")
 
-filename = Path("seviri_product_ids.txt")
-
-api = EumetsatAPI(
-    collection=EumetsatCollection.seviri,
-    log_context="Example [Fetch Product IDs]"
-)
-
-product_batches = api.query_in_batches(
-    start_datetime=datetime(2015, 6, 1),
-    end_datetime=datetime(2015, 8, 1),
+writer = Writer(output_filepath=output_filepath)
+datetime_range_in_batches = DateTimeRangeInBatches(
+    start_datetime="2019-01-01T00:00:00+00:00",
+    end_datetime="2021-01-01T00:00:00+00:00",
     batch_interval=timedelta(days=30)
 )
 
-write_items_to_txt_file_in_batches(product_batches, filename)
+if __name__ == "__main__":
+    product_batches = EumetsatQuery().query_in_batches(datetime_range_in_batches)
+    number_of_items = writer.write_in_batches(product_batches)
+    print("number of items successfully fetched and written to the file: ", number_of_items)
