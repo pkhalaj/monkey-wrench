305720661338ebe21e1109abe59ebec6c1fbdbd8
Pouria Khalaj <pouria.khalaj@smhi.se>
2025-02-24 23:32:36 +0100

    Amend the examples for `verify_resampled_files`

--- examples/verify_resampled_files/script.py
+++ examples/verify_resampled_files/script.py
@@ -1,49 +1,45 @@
-from datetime import datetime
+from datetime import UTC, datetime
 from pathlib import Path
 
-from monkey_wrench.date_time import FilePathParser, SeviriIDParser
-from monkey_wrench.input_output import (
-    compare_files_against_reference,
-    read_items_from_txt_file,
-    visit_files_in_directory,
-)
+from monkey_wrench.date_time import DateTimePeriod, FilePathParser, SeviriIDParser
+from monkey_wrench.input_output import DirectoryVisitor, FilesIntegrityValidator
 from monkey_wrench.query import List
 
-# Check here!
-product_ids_filename = Path("<replace-with-full-path-and-filename-of-the-text-file-in-which-product-ids-are-stored>")
-input_directory = Path("<replace-with-directory-where-the-files-are-stored>")
+reference = Path("<replace_with_the_full_path_of_the_text_file_in_which_product_ids_are_stored>")
+parent_directory_path = Path("<replace_with_the_path_of_the_top_level_directory_where_the_files_are_to_be_stored>")
 
-start_datetime = datetime(2022, 1, 1)
-end_datetime = datetime(2024, 1, 1)
-
-product_ids = List(
-    read_items_from_txt_file(product_ids_filename),
-    SeviriIDParser
-).query(
-    start_datetime,
-    end_datetime
-)
-expected_datetime_instances = [SeviriIDParser.parse(i) for i in product_ids]
-
-collected_files = List(
-    visit_files_in_directory(input_directory),
-    FilePathParser
-).query(
-    start_datetime,
-    end_datetime
-).to_python_list()
-
-missing, corrupted = compare_files_against_reference(
-    collected_files,
-    reference_items=expected_datetime_instances,
-    transform_function=FilePathParser.parse,
-    nominal_size=0,  # replace 0 with the nominal size of a single file in bytes (positive integer!)
-    tolerance=0.01
+datetime_period = DateTimePeriod(
+    start_datetime=datetime(2022, 1, 1, tzinfo=UTC),
+    end_datetime=datetime(2024, 1, 1, tzinfo=UTC)
 )
 
-print({
-    "number of files found": len(collected_files),
-    "number of reference items ": len(expected_datetime_instances),
-    "number of missing files": len(missing),
-    "number of corrupted files": len(corrupted),
-})
+files_validator = FilesIntegrityValidator(
+    nominal_file_size="<replace_with_nominal_size_of_a_single_file_in_bytes_as_an_integer>",
+    file_size_relative_tolerance=0.01,
+    number_of_processes=20,
+    filepath_transform_function=FilePathParser.parse
+)
+
+if __name__ == "__main__":
+    files = List(
+        DirectoryVisitor(parent_directory_path=parent_directory_path).visit(),
+        FilePathParser
+    ).query(
+        datetime_period
+    ).to_python_list()
+
+    reference = List(
+        files_validator.get_reference_items(reference),
+        SeviriIDParser
+    ).query(
+        datetime_period
+    ).parsed_items.tolist()
+
+    missing, corrupted = files_validator.verify_files(files, reference)
+
+    print({
+        "number of files found": len(files),
+        "number of reference items ": len(reference),
+        "number of missing files": len(missing),
+        "corrupted files": corrupted,
+    })
