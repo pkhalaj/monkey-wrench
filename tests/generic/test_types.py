3f7291bdbdf2711059933e83965da23dc1335ccb
Pouria Khalaj <pouria.khalaj@smhi.se>
2025-03-06 15:57:49 +0100

    Add `PathLikeType` to `generic`

--- tests/generic/test_types.py
+++ tests/generic/test_types.py
@@ -1,7 +1,9 @@
+from pathlib import Path
+
 import pytest
 from pydantic import ValidationError
 
-from monkey_wrench.generic import Model
+from monkey_wrench.generic import Model, PathLikeType
 
 # ======================================================
 ### Tests for Model()
@@ -14,3 +16,32 @@ def test_Model():
 
     with pytest.raises(ValidationError):
         ModelNew(field="test", another=2)
+
+
+# ======================================================
+### Tests for PathLikeType()
+
+@pytest.fixture
+def test_model():
+    class Test(Model):
+        field: PathLikeType
+
+    return Test
+
+
+@pytest.mark.parametrize("inp", [
+    "path",
+    "/home/user/path",
+    Path("/home/user/path"),
+])
+def test_PathLikeType(test_model, inp):
+    assert test_model(field=inp).field == Path(inp)
+
+
+@pytest.mark.parametrize("inp", [
+    123,
+    []
+])
+def test_PathLikeType_raise(test_model, inp):
+    with pytest.raises(ValidationError, match="cannot be converted"):
+        test_model(field=inp)
