d973b3e99768360b534afa4734310ad332c864ed
Pouria Khalaj <pouria.khalaj@smhi.se>
2025-04-11 18:26:49 +0200

    Add `strict` as an option to `ChimpRetrieval` model

--- monkey_wrench/chimp/_models.py
+++ monkey_wrench/chimp/_models.py
@@ -1,5 +1,6 @@
 from typing import Callable, Literal
 
+from loguru import logger
 from pydantic import FilePath, NonNegativeInt, PositiveInt
 
 from monkey_wrench.date_time import ChimpFilePathParser
@@ -26,10 +27,21 @@ class ChimpRetrieval(
     tile_size: PositiveInt = 256
     verbose: bool = True
 
+    strict: bool = False
+    """Determines whether an exception must be raised if there are missing timestamps. Defaults to ``False``.
+
+    By default, CHIMP is expected to handle missing timestamps if they are not in the beginning or the end of the
+    sequence. In such cases, we log a warning. However, if this is set to ``True`` we raise an exception instead.
+
+    Warning:
+        It is not 100% guaranteed that a retrieval can always be performed in the absence of some timestamps. There
+        might be edge cases that CHIMP cannot handle.
+    """
+
     def run_in_batches(self, lst: List) -> None:
         """Perform CHIMP retrievals in batches."""
         with seviri_extension_context() as chimp_cli:
-            batches = lst.generate_k_sized_batches_by_index(self.sequence_length)
+            batches = lst.generate_k_sized_batches_by_index(self.sequence_length, strict=False)
 
             for batch in batches:
                 self.__run_for_single_batch(batch, chimp_cli)
@@ -39,10 +51,12 @@ class ChimpRetrieval(
         input_filepaths = [str(i) for i in batch]
 
         if len(input_filepaths) != self.sequence_length:
-            raise ValueError(
-                f"Expected to receive {self.sequence_length} input files "
-                f"but got {len(input_filepaths)} instead!"
-            )
+            msg = f"Expected to receive {self.sequence_length} input files but got {len(input_filepaths)} instead!"
+            msg += f" Batch: {batch}"
+            if self.strict:
+                raise ValueError(msg)
+            logger.warning(msg)
+
         return input_filepaths
 
     def __run_for_single_batch(self, batch: list[FilePath], retrieve_function: Callable) -> None:
@@ -64,10 +78,18 @@ class ChimpRetrieval(
             last_snapshot = ChimpFilePathParser.parse(batch[-1])
             datetime_directory = self.create_datetime_directory(last_snapshot)
 
-            copy_files_between_directories(
+            copied_files = copy_files_between_directories(
                 tmp_dir,
                 datetime_directory,
-                Pattern(sub_strings=str(
-                    output_filename_from_datetime(last_snapshot))
+                Pattern(
+                    sub_strings=str(output_filename_from_datetime(last_snapshot))
                 )
             )
+
+            match len(copied_files):
+                case 0:
+                    logger.error(f"Could not perform a retrieval for {batch}")
+                case 1:
+                    logger.success("Successfully performed a retrieval.")
+                case n:
+                    logger.error(f"Expected a single file for the retrieval but copied {n} files. Batch: {batch}")
