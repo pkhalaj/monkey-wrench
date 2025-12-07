26d4d39694ee76edf3d3e0928198a2261aa548b0
Pouria Khalaj <pouria.khalaj@smhi.se>
2025-12-07 17:31:19 +0100

    Fix failing test in `test_resampler`
    This includes simplication of tests as well.

--- tests/input_output/seviri/test_resampler.py
+++ tests/input_output/seviri/test_resampler.py
@@ -1,7 +1,5 @@
 from unittest import mock
 
-import pytest
-
 from monkey_wrench.input_output.seviri import RemoteSeviriFile, Resampler
 from tests.geometry.test_models import get_area_definition
 
@@ -11,20 +9,15 @@ from tests.geometry.test_models import get_area_definition
 product_id = "MSG3-SEVI-MSG15-0100-NA-20230413164241.669000000Z-NA"
 
 
-@pytest.fixture
-def fs_file(temp_dir, get_token_or_skip):
+def test_RemoteSeviriFile(temp_dir, get_token_or_skip):
     fs_file = RemoteSeviriFile(fsspec_cache="filecache").open(product_id, temp_dir)
-    return fs_file
-
-
-def test_RemoteSeviriFile(fs_file):
     assert f"{product_id}.nat" == fs_file.open().name
 
 
 # ======================================================
 ### Tests for Resampler()
 
-def test_Resampler(temp_dir, fs_file):
+def test_Resampler(temp_dir, get_token_or_skip):
     scene = mock.MagicMock()
     scene.load = mock.MagicMock()
     resampled_scene = mock.MagicMock()
@@ -41,4 +34,6 @@ def test_Resampler(temp_dir, fs_file):
             filename=f"{temp_dir}/2023/04/13/seviri_20230413_16_42.nc",
             **resampler.dataset_save_options
         )
-        scene_class.assert_called_once_with([fs_file], "seviri_l1b_native")
+        [file_list, reader], _ = scene_class.call_args
+        assert str(file_list) == '[<FSFile "MSG3-SEVI-MSG15-0100-NA-20230413164241.669000000Z-NA.nat">]'
+        assert reader == "seviri_l1b_native"
